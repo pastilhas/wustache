b83dd6ee38eaae929d5fc9b44900783d574d7fc5
Pastilhas <joao.campos1221@gmail.com>
2024-09-24 17:50:32 +0100

    add tests

--- src/render_test.v
+++ src/render_test.v
@@ -23,3 +23,39 @@ fn test_map() {
 	res := render(template, context)!
 	assert res == ' 1337 53.5 '
 }
+
+fn test_missing_value() {
+	context := '{ "a": 50 }'
+	template := '{{game}}'
+
+	render(template, context) or {
+		assert err.str() == 'Missing value game'
+		return
+	}
+
+	assert false
+}
+
+fn test_empty_tag() {
+	context := '{ "a": 50 }'
+	template := '{{}}'
+
+	render_with(template, context, Opts{allow_empty_tag:false}) or {
+		assert err.str().starts_with('Empty tag at')
+		return
+	}
+
+	assert false
+}
+
+fn test_iterator_after_array() {
+	context := '{ "a": [1337, 42, 50] }'
+	template := '{{#a}}{{$}} {{/a}} {{$}}'
+
+	render(template, context) or {
+		assert err.str() == 'Missing value $'
+		return
+	}
+
+	assert false
+}
